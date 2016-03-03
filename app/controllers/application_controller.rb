d554b8fb2c3bca3071741d9c3df1471393f2957a
戴鑫通 <xintong.dai@wenba100.com>
2016-03-03 17:55:53 +0800

    update
    Signed-off-by: 戴鑫通 <xintong.dai@wenba100.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,8 @@ class ApplicationController < ActionController::Base
   def nokogiri_parse string, url = nil, encoding = nil, options = nil
     doc = Nokogiri.parse string, url, encoding, options
     doc.css('script, iframe, frameset').remove   # must same with ApplicationHelper
+    doc.css('a').each{|a|a.remove_attribute 'href'}
+    doc.css('a').each{|a|a.set_attribute 'onclick', 'return false;'}
     doc
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,8 @@ module ApplicationHelper
   def nokogiri_parse string, url = nil, encoding = nil, options = nil
     doc = Nokogiri.parse string, url, encoding, options
     doc.css('script, iframe, frameset').remove   # must same with ApplicationController
+    doc.css('a').each{|a|a.remove_attribute 'href'}
+    doc.css('a').each{|a|a.set_attribute 'onclick', 'return false;'}
     doc
   end
 end
